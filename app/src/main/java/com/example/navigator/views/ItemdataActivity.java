76a0515d8d0c4306ad56b9e95fc67517f7ef996b
Weezya <nguema@et.esiea.fr>
2019-09-24 17:00:14 +0200

    ListView d'items + description de chaque item sélectionné (items stockés en dur)

--- app/src/main/java/com/example/navigator/views/ItemdataActivity.java
+++ app/src/main/java/com/example/navigator/views/ItemdataActivity.java
@@ -1,14 +1,34 @@
 package com.example.navigator.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.example.navigator.R;
 
 public class ItemdataActivity extends AppCompatActivity {
+
+    TextView name;
+    TextView desc;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itemdata);
+
+        getSupportActionBar().setTitle("Informations");
+
+        name=findViewById(R.id.itemdataname);
+        desc=findViewById(R.id.itemdatadesc);
+        image=findViewById(R.id.itemdataimg);
+
+        Intent intent = getIntent();
+
+        name.setText(intent.getStringExtra("name"));
+        desc.setText(intent.getStringExtra("desc"));
+        image.setImageResource(intent.getIntExtra("image",0));
     }
 }
--- app/src/main/java/com/example/navigator/views/MainActivity.java
+++ app/src/main/java/com/example/navigator/views/MainActivity.java
@@ -13,10 +13,44 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.navigator.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ListView listView;
+
+    String[] itemName = {
+            "Métropole",
+            "Rocade",
+            "Collines",
+            "Viaduc",
+            "Plage",
+            "Étoiles"};
+
+    String[] itemDesc = {
+            "Au cœur de la ville, avec ses flux et ses quartiers",
+            "Contourner la côte en passant par ce magnifique chemin digne de Vice City",
+            "Un endroit reculé caché entre des collines, idéal pour se ressourcer et se mettre au vert",
+            "Magnifique pont où l'on peut admirer le cours d'eau, conçu par les plus grands ingénieurs",
+            "Venir se baigner en famille ou avec ses amis et profiter du soleil pour s'amuser ou faire bronzette, sans oublier son maillot de bain",
+            "Admirer les étoiles dans un espace loin de la ville et se griller quelques guimauves autour d'un feu"};
+
+    int[] itemImg = {
+            R.drawable.city,
+            R.drawable.beach,
+            R.drawable.mountain,
+            R.drawable.bridge,
+            R.drawable.plage,
+            R.drawable.sky};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +58,25 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Navigator Beta by Loan");
+        listView=findViewById(R.id.listviewshow);
+        MyAdapter myadapter = new MyAdapter();
+        listView.setAdapter(myadapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ItemdataActivity.class);
+                intent.putExtra("name", itemName[position]);
+                intent.putExtra("image", itemImg[position]);
+                intent.putExtra("desc", itemDesc[position]);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this,"Work in Progress", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,5 +147,37 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return itemImg.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.item_row_data,null);
+
+            TextView name = view1.findViewById(R.id.itemNameRow);
+            TextView desc = view1.findViewById(R.id.itemDescRow);
+            ImageView image = view1.findViewById(R.id.itemImgRow);
+
+            name.setText(itemName[position]);
+            desc.setText(itemDesc[position]);
+            image.setImageResource(itemImg[position]);
+
+            return view1;
+        }
     }
 }
