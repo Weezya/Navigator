2a371584fe0bcc215abf8eff723bc4b6a264ccf4
Weezya <nguema@et.esiea.fr>
2019-09-24 15:36:23 +0200

    Structuration MVC

--- app/src/main/java/com/example/navigator/views/MainActivity.java
+++ app/src/main/java/com/example/navigator/views/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.navigator;
+package com.example.navigator.views;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,10 +9,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import com.example.navigator.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
---
+++ app/src/main/java/com/example/navigator/views/ResaActivity.java
@@ -0,0 +1,14 @@
+package com.example.navigator.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.navigator.R;
+
+public class ResaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resa);
+    }
+}
